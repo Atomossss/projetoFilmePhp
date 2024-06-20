c8aeccee746d54412fbdad37db4e317325957a9c
Fernanda costa <fernandinhacosta.97@hotmail.com>
2024-06-20 20:55:21 -0300

    home e login separados

--- menu/paginas/index.php
+++ menu/paginas/index.php
@@ -12,61 +12,12 @@
  <h2>Seu site confiavel de indicaçoes de filmes <br>
     atualizados diaria mente com as melhoes dicas de filmes.<br>
     acesse ja e deixe sua avaliação e comentarios</h2>
-<?php
-//Talvez colocar type password na senha  
-//Início da sessão do usuário
-    session_start();
-    
-    require_once "../sistema/usuarios.php";
     
 
-    $usuarioSessao = $_SESSION["usuario"] ?? null;
-    $senha = $_SESSION["senha"] ?? null;
-    
-    if(!is_null($usuarioSessao)){
+<div >
+<a href="login.php" >Fazer login</a>
 
-      //  echo" FUNCIONOU AEEEEE";
-        $nome = $_SESSION["nome"] ?? null;
-        $permissao = $_SESSION["permissao"] ?? null;
-        if(!is_null($nome)){
-            echo "<h2>Bem Vindo ". ucfirst(strtolower($nome)) . " </h2>";
-        }
-        if($permissao == 1){
-            require_once "admin.php";
-            header("Location: admin.php");
-        } else {
-            require_once "membro.php";
-            header("Location: membro.php");
-        }
-    }else{
-        include_once "formulario.php";
-
-        $usuario = $_POST["usuario"] ?? null;
-        $senha =  $_POST["senha"] ?? null;
-
-        if(is_null($usuario) && is_null($senha)){
-            //echo " Fazer Login";
-            include_once "formulario.php";
-        }else{
-
-            if(verificarUser($usuario, $senha)){
-              //  echo "Fazendo Login... Entrando na Conta :)";
-                header('Location: '.$_SERVER['REQUEST_URI']);
-              
-            }else{
-                echo "Senha inválida";
-                    // fazer var dump
-                echo "Tentar Novamente";
-
-                require_once "formulario.php";
-            }
-            
-        }
-
-    }
-
-
-?>
+</div>
 
 
 </body>
